5a2b4669c5fe85df1290b7e04988eb7ac94b57bd
abdoHossam <abdohossam120@gmail.com>
2021-10-24 01:07:58 +0200

    add user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,21 @@ class UserSeeder extends Seeder
             'email' => 'abdo@admin.com',
             'password' => Hash::make('12345'),
             'role_id' => 1,
+            'email_verified_at' => "2021-10-01 18:59:26",
+        ]);
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345'),
+            'role_id' => 2,
+            'email_verified_at' => "2021-10-01 18:59:26",
+        ]);
+        User::create([
+            'name' => 'user',
+            'email' => 'user@admin.com',
+            'password' => Hash::make('12345'),
+            'role_id' => 3,
+            'email_verified_at' => "2021-10-01 18:59:26",
         ]);
     }
 }
