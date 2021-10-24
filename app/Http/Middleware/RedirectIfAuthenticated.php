9b7ab49d6fdf2c1faa62d3c7e2029e41acac8e29
abdoHossam <abdohossam120@gmail.com>
2021-10-25 00:58:55 +0200

    update2

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,10 +23,10 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect(url('/'));
             }
         }
 
-        return redirect(url('/'));
+        return $next($request);
     }
 }
